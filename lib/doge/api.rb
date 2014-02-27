3dd05c9e7663e53cd3955cbf8f7d7bfbaa91840d
Matthew Fisher <matthewf@opdemand.com>
2014-02-27 01:23:02 -0800

    add API version switching

--- lib/doge/api.rb
+++ lib/doge/api.rb
@@ -5,9 +5,10 @@ module DogeApi
   class DogeApi
     attr_accessor :api_key
 
-    def initialize(api_key)
+    def initialize(api_key, version=1)
       @api_key = api_key
-      @base_uri = URI.parse('https://www.dogeapi.com/wow')
+      @api_version = version
+      @base_uri = URI.parse(_get_dogeapi_endpoint())
     end
 
     def build_uri(m, args)
@@ -23,6 +24,14 @@ module DogeApi
 
     def method_missing(m, args = {}, &block)
       fetch_uri build_uri(m, args)
+    end
+
+    private
+
+    def _get_dogeapi_endpoint()
+      base_uri = 'https://www.dogeapi.com/wow'
+      base_uri += '/v2' if @api_version == 2
+      return base_uri
     end
   end
 end
